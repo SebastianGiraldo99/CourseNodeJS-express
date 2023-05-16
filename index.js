1258e7d8b47b4e10d02259e4ebaa7aa494d316d6
Sebastian Giraldo <sebastiangg990407@gmail.com>
2023-05-16 18:53:07 -0500

    modificado el archivo index,con algunos ejemplos de rutas

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const Product1 = {
+  name : 'Product 1 ',
+    price : 1000,
+    quatity : 10
+}
 const app = express();
 
 const port = 3000;
 
+/**
+ * Default route
+ */
 app.get('/', (req, res) => {
     res.send('This is my first server in node');
 });
+/**
+ * Other route
+ */
+app.get('/new-route', (req, res) => {
+  res.send("This is other route, it's a personalizate route");
+});
+
+/**
+ * the result of this route is a JSON
+ */
+app.get('/products', (req, res) => {
+  res.json({
+    Product1
+  });
+});
+
+app.get('/categories', (req, res) => {
+  res.json({
+    name : 'Categorie 1',
+    products : [Product1]
+  });
+});
+
 
 app.listen(port, () =>{
     console.log('This is the port of my app: ' + port);
