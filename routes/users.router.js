63c41c4035afc12238fbf8c5ccc9a654dd2d3ffd
Sebastian Giraldo <sebastiangg990407@gmail.com>
2023-06-12 14:40:07 -0500

    Agregado de nuevos paquetes, archivo de configuracion de docker

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const UserService = require('../services/user.service');
 
 
 const router = express.Router();
+const service = new UserService();
 /**
  * QUERY PARAMS
  *
  */
-router.get('/', (req,res) =>{
+router.get('/', async(req,res, next) =>{
+  try{
+    const users = await service.find();
+    res.json(users);
+  }
+  catch (error){
+    next (error);
+  }
+});
+
+router.get('/params', (req,res) =>{
   const {limit, offset} = req.query;
   if(limit && offset){ //Validamos que existan esos parametros.
     res.json({
