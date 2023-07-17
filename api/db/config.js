c7cdebad04ad51ba2c00b9d66dd489191b3beeee
Sebastian Giraldo <sebastiangg990407@gmail.com>
2023-07-17 17:39:47 -0500

    creacion de las migraciones y agregado del sql al docker

--- api/db/config.js
+++ api/db/config.js
@@ -1,22 +1,18 @@
-const {Sequelize} = require('sequelize');
-
-
 const {config} = require('../config/config');
-const setupModels = require('../db/models');
+
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
+module.exports = {
+  development : {
+    url : URI,
+    dialect : 'postgres',
+  },
+  production : {
+    url : URI,
+    dialect : 'postgres',
+  }
 
-const sequeleze = new Sequelize(URI,
-    {
-      dialect : 'postgres',
-      logging : true,
-
-    });
-setupModels(sequeleze);
-
-
-
-module.exports = sequeleze;
+}
