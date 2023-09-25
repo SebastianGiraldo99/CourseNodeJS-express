cdc55ebe6d4bcd6669ef94da3c1a1790cda0c0c6
Sebastian Giraldo <sebastiangg990407@gmail.com>
2023-09-25 11:36:06 -0500

    preparacion para el despliegue en servidor

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -4,17 +4,20 @@ const {Sequelize} = require('sequelize');
 const {config} = require('../config/config');
 const setupModels = require('../db/models');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const options = {
+    dialect : 'postgres',
+    logging : config.isProd ? false : true,
 
+}
+if(config.isProd){
+  options.dialectOptions = {
+    ssl : {
+      rejectUnauthorized : false
+    }
+  }
+}
 
-const sequeleze = new Sequelize(URI,
-    {
-      dialect : 'postgres',
-      logging : true,
-
-    });
+const sequeleze = new Sequelize(config.dbUrl, options);
 setupModels(sequeleze);
 
 
